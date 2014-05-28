889540def8b98a279c60b53fa737bff8eab4394a
Brent Lee <brent@opentravelexchange.net>
2014-05-28 17:59:34 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'f3d01e004ae67499b32d858789b46bfc34c01540cc08591884f141ab683f9ea34705e1e6e08c50541e105aa42d8c536353d56390c8181b3a71f50f906bf244b9'
